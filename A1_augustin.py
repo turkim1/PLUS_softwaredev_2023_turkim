f95ae2e835febfd08e28f0b0c83352bfcb22b936
Hannah <hannah.augustin@gmail.com>
2023-05-23 13:04:57 +0200

    Create A1_augustin.py

---
+++ A1_augustin.py
@@ -0,0 +1,29 @@
+"""A1
+Merge this pull request for full points. :)
+"""
+#
+#
+### Quote:
+#
+# "Attention is the rarest and purest form of generosity." -- Simone Weil
+#
+#
+### Disliked food/flavour:
+#
+# Crunchy celery. Like nails on a chalkboard.
+#
+### Recommenation:
+#
+# "Ways of Being: Animals, Plants, Machines: The Search for a Planetary Intelligence " by James Bridle
+#
+'''Another quote from the recommended book:
+
+“Imagine a system with clearly defined goals, sensors and effectors for reading and interacting with the world, the ability to recognize pleasure and pain as attractors and things to avoid, the resources to carry out its will, the legal and social standing to see that its needs are catered for, even respected. That's a description on an AI - it's also a description of a modern corporation.”
+― James Bridle, Ways of Being: Animals, Plants, Machines: The Search for a Planetary Intelligence 
+'''
+#
+#
+### German word:
+#
+# "Mahlzeit" Austrian for "enjoy your meal", but also used as a greeting to say hello to anyone around lunchtime (11-14:00)
+#
